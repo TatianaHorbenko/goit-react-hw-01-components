ff1d35aaf43fc59ab41673218b77e779818c0d4d
TatianaHorbenko <3435640@gmail.com>
2022-06-13 14:32:45 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import Profile from './Profile';
+import Statistic from './Statistics';
+import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
+
+import user from '../dataFiles/user.json';
+import data from '../dataFiles/data.json';
+import friends from '../dataFiles/friends.json';
+import transactions from '../dataFiles/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistic title="Upload stats" stats={data} />
+      <FriendList options={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
